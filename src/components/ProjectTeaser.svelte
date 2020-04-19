01917b9cea4389e90ab0347844e02eaa42558a45
Stephan <stephan.simonett@gmail.com>
2020-04-19 23:35:52 +0200

    Refacture project teaser

--- src/components/ProjectTeaser.svelte
+++ src/components/ProjectTeaser.svelte
@@ -1,122 +1,41 @@
 <script>
-export let image
-export let title
-export let desc
-export let icons
-export let url
-export let repo
-export let locked
+  export let image;
+  export let title;
+  export let categories;
+  export let url;
+  export let gutter;
+  export let index;
 
+  import { overlayIsOpen } from "../stores.js";
+  import { project } from "../stores.js";
+
+  function openOverlay() {
+    overlayIsOpen.update(overlayIsOpen => {
+      if (!overlayIsOpen) {
+        return (overlayIsOpen = true);
+      }
+      if (overlayIsOpen) {
+        return (overlayIsOpen = false);
+      }
+    });
+    project.update(value => (value = index));
+  }
 </script>
 
 <style>
-.project-teaser {
-    padding: 1rem;
-    border-radius: 4px;
-    -webkit-box-shadow: 0 0 0 1px rgba(0,0,0,.06), 0 4px 6px -4px rgba(0,0,0,.15);
-    box-shadow: 0 0 0 1px rgba(0,0,0,.06), 0 4px 6px -4px rgba(0,0,0,.15);
-    background-color: #fff;
-    transition: all 80ms ease-in;
-    margin-bottom: 2rem;
-}
-@media (min-width: 800px) {
-    .project-teaser {
-        flex: 0 1 calc(50% - 3rem);
-    }
-}
-.project-teaser:hover {
-    -webkit-box-shadow: 0 6px 12px -2px rgba(50,50,93,.25), 0 3px 7px -3px rgba(0,0,0,.3);
-    box-shadow: 0 6px 12px -2px rgba(50,50,93,.25), 0 3px 7px -3px rgba(0,0,0,.3);
-    transition: all 80ms ease-in;
-}
-
-.project-teaser img {
-    width: calc(100% + 4rem);
-    height: auto;
-    margin: -2rem -2rem 0 -2rem;
-}
-/* .devicons :global(.devicon-javascript-plain:hover) {
-    color: #f0db4f;
-}
-.devicons :global(.devicon-wordpress-plain:hover) {
-    color: #494949;
-}
-.devicons :global(.devicon-php-plain:hover) {
-    color: #6181b6;
-}
-.devicons :global(.devicon-stripe-plain:hover path),
-.devicons :global(.devicon-stripe-plain:hover polygon) {
-    fill: #32325D;
-} */
-
-:global(.devicons .project-teaser .devicon-stripe-plain g path),
-:global(.devicons .project-teaser .devicon-stripe-plain g polygon) {
-    fill: #505050;
-}
-.devicons {
-    display: block;
-    float: left;
-    padding: 0 1rem 0 0;
-    font-size: 3rem;
-    color: #505050;
-}
-.devicons-list {
-    padding: 0
-}
-.devicons-list::after {
-    content: '';
-    display: block;
-    clear: both;
-}
-.devicons :global(i) {
-    transition: all 80ms ease-in;
-}
-.links {padding: 0;}
-.links li {
-    list-style: none;
-}
-.links li a {
-    text-decoration: none;
-    float: left;
-    margin-right: 2rem;
-}
-hr {
-    border: none;
-    border-top: 1px solid #e1e1e1;
-}
+  img {
+    transition: all 0.2s ease-in-out;
+  }
+  img:hover {
+    transform: scale(1.01);
+  }
 </style>
 
-<div class="project-teaser link-slideright">
-    <img class="project-image" src="{image}" alt="Screenshot of the website">
-    <h3>{title}</h3>
-    <p>{desc}</p>
-    <hr>
-    <h4>Technology used</h4>
-    <ul class="devicons-list">
-        {#each icons as icon}
-            <li class="devicons">
-                {@html icon}
-            </li>
-        {/each}
-    </ul>
-    <ul class="links">
-        <li><a href="{url}" target="_blank" rel="noopener">
-            Site
-        </a></li>
-
-        {#if locked}
-            <li>
-                <a href aria-disabled="true" rel="noopener">
-					<span class="icon-lock"></span>
-					Github
-			    </a>
-            </li>
-        {:else}
-            <li>
-                <a href="{repo}" target="_blank" rel="noopener">
-                    Github
-                </a>
-            </li>
-        {/if}
-    </ul>
-</div>
+<button
+  on:click={openOverlay}
+  style="width: calc(50% - {gutter}px);"
+  class="cursor-pointer focus:outline-none odd:mt-32">
+  <img class="mt-32" src={image} alt={title} />
+  <h3 class="font-light text-3xl text-center mt-8">{title}</h3>
+  <p class="font-light text-lg text-center mt-2">{categories}</p>
+</button>
