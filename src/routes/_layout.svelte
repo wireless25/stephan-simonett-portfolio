a42cefda59907a48ae08c18c58cdea9e725d7200
Stephan <stephan.simonett@gmail.com>
2020-04-19 23:34:51 +0200

    Remove unused styles

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,35 +1,14 @@
 <script>
-	import Nav from '../components/Nav.svelte';
-	import Footer from '../components/Footer.svelte';
+  import Nav from "../components/Nav.svelte";
+  import Footer from "../components/Footer.svelte";
 
-	export let segment;
+  export let segment;
 </script>
 
-<style>
-	main {
-		position: relative;
-		max-width: 75rem;
-		padding: 1rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-	@media (min-with: 75rem) {
-		main {
-			padding: 0px;	
-		}
-	}
-	#site-border {
-		border-top: solid 4px #0041FF;
-		position: relative;
-		z-index: 1;
-	}
-</style>
-
-<div id="site-border"></div>
-<Nav {segment}/>
+<Nav {segment} />
 
 <main>
-	<slot></slot>
+  <slot />
 </main>
 
 <Footer />
