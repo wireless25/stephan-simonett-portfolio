ea0383f5fe5612b19d999d1b5c554fea016efd66
Stephan <stephan.simonett@gmail.com>
2020-04-19 23:32:16 +0200

    Add about section component

---
+++ src/components/AboutSection.svelte
@@ -0,0 +1,23 @@
+<section id="about" class="mt-32 pb-32 relative">
+  <div
+    id="backdrop"
+    class="absolute left-0 w-full right-0 bottom-0 top-1/6 bg-gray-50" />
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <img class="w-1/2 relative" src="/about-me.jpg" alt="Stephan Simonett" />
+    <div class="flex -mt-1/6 relative z-50">
+      <div class="w-3/12" />
+      <div class="p-24 shadow-2xl w-9/12 bg-white">
+        <h2 class="text-6xl">
+          <span class="font-black">About</span>
+          <span class="font-light">me</span>
+        </h2>
+        <p class="text-4xl text-gray-500 font-light">I am this and that</p>
+        <p class="text-2xl mt-10">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
