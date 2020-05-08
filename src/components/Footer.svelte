13ab332714eb49188168a8a5ccf2bcb90bdd8802
Stephan <stephan.simonett@gmail.com>
2020-05-08 14:56:27 +0200

    Refacture footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,6 @@
 <footer
-  class="bg-blue-brand text-white text-lg text center w-full px-4 sm:px-6
-  lg:px-8 py-16 mt-32">
+  class="bg-blue-brand text-white text-base md:text-lg text center w-full px-4
+  sm:px-6 lg:px-8 py-8 md:py-16 mt-20 md:mt-32">
   <div class="footer-content">
     <p class="text-center">
       © Stephan Simonett | stephansimonett.ch | LinkedIn | Github |
