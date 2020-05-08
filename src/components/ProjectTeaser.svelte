f6178c14381a250bfe43730660c475b30efafa7e
Stephan <stephan.simonett@gmail.com>
2020-05-08 14:56:58 +0200

    Refacture project teaser

--- src/components/ProjectTeaser.svelte
+++ src/components/ProjectTeaser.svelte
@@ -1,15 +1,32 @@
 <script>
-  export let image;
-  export let title;
-  export let categories;
-  export let url;
-  export let gutter;
-  export let index;
-
+  import { onMount } from "svelte";
   import { overlayIsOpen } from "../stores.js";
   import { project } from "../stores.js";
 
+  export let image;
+  export let title;
+  export let categories;
+  export let gutter;
+  export let index;
+
+  let teaser;
+
+  const setTeaserWidth = () => {
+    const width = window.innerWidth;
+    width >= 768
+      ? (teaser.style.width = `calc(50% - ${gutter}px)`)
+      : (teaser.style.width = "100%");
+    teaser.classList.add("block");
+    teaser.classList.remove("hidden");
+  };
+
+  onMount(() => {
+    setTeaserWidth();
+    window.addEventListener("resize", setTeaserWidth);
+  });
+
   function openOverlay() {
+    project.update(value => index);
     overlayIsOpen.update(overlayIsOpen => {
       if (!overlayIsOpen) {
         return (overlayIsOpen = true);
@@ -18,7 +35,6 @@
         return (overlayIsOpen = false);
       }
     });
-    project.update(value => (value = index));
   }
 </script>
 
@@ -32,10 +48,14 @@
 </style>
 
 <button
+  bind:this={teaser}
   on:click={openOverlay}
-  style="width: calc(50% - {gutter}px);"
-  class="cursor-pointer focus:outline-none odd:mt-32">
-  <img class="mt-32" src={image} alt={title} />
-  <h3 class="font-light text-3xl text-center mt-8">{title}</h3>
-  <p class="font-light text-lg text-center mt-2">{categories}</p>
+  class="hidden cursor-pointer focus:outline-none md:odd:mt-32">
+  <img class="mt-8 md:mt-32" src={image} alt={title} />
+  <h3 class="font-light text-2xl md:text-3xl text-center mt-4 md:mt-8">
+    {title}
+  </h3>
+  <p class="font-light text-base md:text-lg text-center mt-1 md:mt-2">
+    {categories}
+  </p>
 </button>
