d99af53805ce69b76f6bc6f7e8923758c1675c36
Stephan <stephan.simonett@gmail.com>
2020-05-08 15:09:25 +0200

    Add content from API

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,25 @@
+<script context="module">
+  export async function preload() {
+    const res = await this.fetch(`${process.env.VERCEL_URL}/api/projects`);
+    const projects = await res.json();
+
+    return { projects };
+  }
+</script>
+
 <script>
   import { onMount } from "svelte";
   import MagicGrid from "magic-grid";
   import ProjectTeaser from "../components/ProjectTeaser.svelte";
-  import content from "../content.js";
   import ContentOverlay from "../components/ContentOverlay.svelte";
   import AboutSection from "../components/AboutSection.svelte";
   import ContactSection from "../components/ContactSection.svelte";
-  import { overlayIsOpen } from "../stores.js";
-  import { project } from "../stores.js";
+  import { overlayIsOpen, project, content } from "../stores.js";
 
   let grid;
   let gutter = 40;
+
+  export let projects = {};
 
   onMount(() => {
     let magicGrid = new MagicGrid({
@@ -43,25 +52,26 @@
   <title>Stephan Simonett | PM and Frontend Developer</title>
 </svelte:head>
 
-<svelte:component this={ContentOverlay} {...content.projects[$project]} />
+<svelte:component this={ContentOverlay} {...projects[$project]} />
 
 <div>
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    <h1 class="text-2xl lg:text-7xl mt-32">
-      {@html content.title}
+    <h1 class="text-4xl mt-12 lg:text-7xl lg:mt-32">
+      {@html $content.project.headline}
     </h1>
   </div>
 
-  <section id="projects" class="pb-48 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+  <section
+    id="projects"
+    class="md:pb-48 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 min-h-screen">
     <div bind:this={grid}>
 
-      {#each content.projects as project, i}
+      {#each projects as project, i}
         <ProjectTeaser
           image={project.image}
           title={project.title}
           {gutter}
           categories={project.categories}
-          url={project.url}
           index={i} />
       {/each}
 
